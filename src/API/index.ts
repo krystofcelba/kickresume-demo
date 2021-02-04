3b2072c1a9e84c0525d452f3191b96f4a45d5c55
Krystof Celba <krystof.celba@icloud.com>
2021-02-04 02:11:54 +0100

    feat: implement API calls

---
+++ src/API/index.ts
@@ -0,0 +1,69 @@
+import axios from "axios";
+import { Constants } from "src/assets";
+
+const APIClient = axios.create({
+  baseURL: Constants.baseURL,
+});
+
+function setAuthHeaders(authToken?: string) {
+  APIClient.defaults.headers.common.Authorization = authToken
+    ? `Token ${authToken}`
+    : undefined;
+}
+
+async function register(data) {
+  const resp = await axios({
+    method: "post",
+    url: `${Constants.baseURL}/register/`,
+    data,
+  });
+
+  return resp.data;
+}
+
+async function login(data) {
+  const resp = await axios({
+    method: "post",
+    url: `${Constants.baseURL}/auth-token/`,
+    data,
+  });
+
+  return resp.data.token;
+}
+
+async function createCV() {
+  console.log("create");
+  const resp = await APIClient.post(
+    "https://test1.kickresume.com/api/resumes/"
+  );
+
+  return resp.data;
+}
+
+async function updateCV(cv_id, data) {
+  console.log("create");
+  const resp = await APIClient.put(
+    `https://test1.kickresume.com/api/cvs/${cv_id}/`,
+    data
+  );
+
+  return resp.data;
+}
+
+async function getPDFUrl(cv_id) {
+  console.log("getting pdf");
+  const resp = await APIClient.get(
+    `https://test1.kickresume.com/api/cvs/${cv_id}/pdf/`
+  );
+
+  return resp.data;
+}
+
+export default {
+  register,
+  login,
+  setAuthHeaders,
+  createCV,
+  updateCV,
+  getPDFUrl,
+};
---
+++ src/assets/index.ts
@@ -0,0 +1,4 @@
+export const Constants = {
+  authTokenKey: "authToken",
+  baseURL: "https://test1.kickresume.com/api",
+};
